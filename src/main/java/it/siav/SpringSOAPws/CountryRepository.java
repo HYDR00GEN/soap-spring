3b0d626082ad2289e2e37468ceb3d2f5dcf0a68a
Paunas Ovidiu <OPaunas@siav.it>
2023-01-19 10:29:20 +0100

    comment repo

--- src/main/java/it/siav/SpringSOAPws/CountryRepository.java
+++ src/main/java/it/siav/SpringSOAPws/CountryRepository.java
@@ -9,11 +9,22 @@ import java.util.Map;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+/*
+Component annotation is used to indicate that a class is a component in Spring framework.
+It is used to mark a class as a candidate for component-scanning, which allows Spring to
+automatically discover and register the class as a bean in the application context.
+*/
 @Component
 public class CountryRepository {
     private static final Map<String, Country> countries = new HashMap<>();
 
-    //
+    /*
+    The "@PostConstruct" annotation is used to indicate a method that should be executed after the object is created
+     and all dependencies have been injected.
+     In the example, the "initData" method is decorated with the "@PostConstruct" annotation, meaning that it will be
+     executed automatically by the Spring framework after the CountryRepository object is created. In this case, the
+     method is used to populate the "countries" map with some sample data.
+     */
     @PostConstruct
     public void initData() {
         Country spain = new Country();
