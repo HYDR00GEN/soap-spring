607ceb9b52181b6d18c84ac0fcff6d1f70f3212e
Paunas Ovidiu <OPaunas@siav.it>
2023-01-19 09:58:15 +0100

    init

---
+++ src/main/java/it/siav/SpringSOAPws/SpringSoaPwsApplication.java
@@ -0,0 +1,12 @@
+package it.siav.SpringSOAPws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoaPwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoaPwsApplication.class, args);
+	}
+}
---
+++ src/test/java/it/siav/SpringSOAPws/SpringSoaPwsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.siav.SpringSOAPws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoaPwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
